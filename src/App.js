c65dda3d413bbef4798e11f8411cc3918642ba29
Peyton Duncan <peyton.duncan45@gmail.com>
2018-07-23 23:00:26 -0400

    Got a table working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import TestDataTable from './TestDataTable';
 import './App.css';
 
 class App extends Component {
@@ -10,16 +11,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        <h1>This shows that CI works!</h1>
         <p className="App-intro">
-          <h1>This shows that CI works!</h1>
           To get started, edit <code>src/App.js</code> and save to reload.
           <br/>
           Now I have updated this site!
           <br/>
           Now I have again!
-          <h1>Let's put in a youtube video for safe measure!</h1>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/V4MF2s6MLxY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <strong>Let's put in a youtube video for safe measure!</strong>
+          {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/V4MF2s6MLxY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> */}
         </p>
+        <TestDataTable/>
       </div>
     );
   }
---
+++ src/TestDataTable.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+import './TestDataTable.css'
+import axios from 'axios'
+
+export default class TestDataTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.reload = this.reload.bind(this)
+
+        this.state = {
+            data: [],
+            columns: [
+                {
+                    Header: "Name",
+                    accessor: "name"
+                },{
+                    Header: "Color",
+                    accessor: "color"
+                }
+            ],
+            loading: false
+        }
+    }
+    reload() {
+        this.setState({loading:true})
+        axios({
+            url: '/api/TestData',
+            method: 'GET'
+        })
+            .then(result => {
+                console.table(result.data.Items)
+                this.setState({ data: result.data.Items, loading:false});
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        return
+    }
+
+    componentDidMount() {
+        this.reload()
+    }
+
+    render() {
+            return (
+                <div className="table-view">
+                    <ReactTable
+                        loading={this.state.loading}
+                        data={this.state.data}
+                        columns={this.state.columns}
+                        className="-striped -highlight"
+                        minRows={10}
+                    />
+                </div>
+            )
+    }
+}
