02f274ff1905fd5fc698a65593710fcf79ce9f22
Peyton Duncan <peyton.duncan45@gmail.com>
2018-09-28 12:32:01 -0400

    Redesign

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import TestDataTable from './TestDataTable';
+// import logo from './logo.svg';
+// import TestDataTable from './TestDataTable';
+import { Button, Alert, Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-
-        <h1>This shows that CI works!</h1>
+        <Alert color="primary">This is a DEVELOPMENT TEST SITE</Alert>
+        {/* <h1>This shows that CI works!</h1>
         <h2>This is a dog picture!</h2>
         <h3>Hi Peter</h3>
-        <img href="https://www.dogbreedinfo.com/images27/CorgidorYellowLabWelshCorgiMixHybridDogAnnabelle12YearsOld.jpg"/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <br/>
-          Now I have updated this site!
-          <br/>
-          Now I have again!
-          <strong>Let's put in a youtube video for safe measure!</strong>
-          <br />
-          <iframe title="hackerman" width="560" height="315" src="https://www.youtube.com/embed/KEkrWRHCDQU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        </p>
-        {/* <TestDataTable/> */}
+        <img href="https://www.dogbreedinfo.com/images27/CorgidorYellowLabWelshCorgiMixHybridDogAnnabelle12YearsOld.jpg" /> */}
+        <div>
+          <Card>
+            <CardBody>
+              <CardTitle>Video</CardTitle>
+              <CardSubtitle>Hackerman Video</CardSubtitle>
+            </CardBody>
+            <CardBody>
+              <CardText><iframe title="hackerman" width="560" height="315" src="https://www.youtube.com/embed/KEkrWRHCDQU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></CardText>
+              <Button onClick={()=>{window.location="https://www.youtube.com/watch?v=KEkrWRHCDQU"}}>LINK</Button>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     );
   }
